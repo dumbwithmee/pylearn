596292a2a71bbb6da2e7a294e3de65da7714c91f
dumbwithmee <dumbwithmee@gmail.com>
2025-04-25 13:11:01 +0900

    get radius

--- hw4.py
+++ hw4.py
@@ -1,18 +1,16 @@
 def rep_char(c, n):
     print(c * n)
 
-
-def draw_line_string(msg1, msg2):
-    nstr = len(msg1) if len(msg1) > len(msg2) else len(msg2)
-
-    print('input his/her name :', msg1)
-    rep_char('-', nstr)
-    print(f'Hello, {msg1}')
+def draw_line_string(name):
+    print(f'input his/her name : {name}')
+    rep_char('--', len(name))
+    print(f'Hello, {name}')
     print('Welcome to Seoul')
-    rep_char('-', nstr)
-
+    rep_char('----', len(name))
+    print()  # 줄 바꿈
 
 # 주 프로그램
 m1 = input('input his/her name1 : ')
 m2 = input('input his/her name2 : ')
-draw_line_string(m1, m2)
+draw_line_string(m1)
+draw_line_string(m2)
