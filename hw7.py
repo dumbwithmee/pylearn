e048f3a649c6e8b40798a792d6152d9577d42684
dumbwithmee <dumbwithmee@gmail.com>
2025-05-02 17:49:01 +0900

    get radius

---
+++ hw7.py
@@ -0,0 +1,16 @@
+#9-4
+# 딕셔너리를 사용하여 수량을 저장
+# 장바구니 내 상품을 검색하여 해당 상품의 수량 출력
+
+shopping_bag = {}
+
+while True:
+    item = input('상품명? : ')
+    if item == '':
+        break
+    qty = int(input('수량은? : '))
+    shopping_bag[item] = qty
+    print(f'장바구니에 {item} {qty}개가 담겼습니다.')
+
+print(f'\n>>> 장바구니 보기 : {shopping_bag}')
+
